058242c9bcc9ea14d1299216f92343c6c8ece1a4
barti777 <37804664+barti777@users.noreply.github.com>
2019-02-03 19:01:03 +0100

    commit

--- views/DefaultController/index.php
+++ views/DefaultController/index.php
@@ -11,11 +11,16 @@
 <div class="container">
     <div class="row">
         <h1 class="col-12">HOMEPAGE</h1>
-        <p>
-            <?= $text ?>
-        </p>
+<!--        <p>-->
+<!--            --><?//= $text ?>
+<!--        </p>-->
+
+        <iframe class="col-12" width = "1085px", height="1827" src="http://vii-lo.krakow.pl/" name="targetframe" allowTransparency="true" scrolling="yes" frameborder="0" >
+        </iframe>
     </div>
 </div>
+
+
 
 </body>
 </html>
